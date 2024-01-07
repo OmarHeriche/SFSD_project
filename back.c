5539989c1aa088bcc0f261b3f51407a008cc7718
omar <herricheomar@gmail.com>
2024-01-07 12:27:23 +0100

    the best creation function

--- back.c
+++ back.c
@@ -7,7 +7,10 @@
 typedef struct cellule *list;
 typedef struct cellule
 {
-    char nom[30], prenom[30], matricule[8], id[30];
+    char nom[30];
+    char prenom[30];
+    char matricule[8];
+    char id[30];
     list svt;
 } cellule;
 
@@ -17,8 +20,38 @@ void allouer(list *nouv)
 }
 
 // todo creating project types: end
-// todo creating project functions:start
-//? start list manipulation functions:start
+void append_in_list(list *start, list *end, char nom[30], char prenom[30], char matricule[8], char id[30])
+{
+    list nouv;
+    if (*start == NULL)
+    {
+        allouer(start);
+        //? set data:start
+        strncat((*start)->nom, nom, sizeof((*start)->nom) - strlen((*start)->nom) - 1);
+        strncat((*start)->prenom, prenom, sizeof((*start)->prenom) - strlen((*start)->prenom) - 1);
+        strncat((*start)->matricule, matricule, sizeof((*start)->matricule) - strlen((*start)->matricule) - 1);
+        strncat((*start)->id, id, sizeof((*start)->id) - strlen((*start)->id) - 1);
+
+        //? set data:end
+        (*start)->svt = NULL;
+        *end = *start;
+    }
+    else
+    {
+        allouer(&nouv);
+        //? set data:start
+        strncat(nouv->nom, nom, sizeof(nouv->nom) - strlen(nouv->nom) - 1);
+        strncat(nouv->prenom, prenom, sizeof(nouv->prenom) - strlen(nouv->prenom) - 1);
+        strncat(nouv->matricule, matricule, sizeof(nouv->matricule) - strlen(nouv->matricule) - 1);
+        strncat(nouv->id, id, sizeof(nouv->id) - strlen(nouv->id) - 1);
+
+        //? set data:end
+        nouv->svt = NULL;
+        (*end)->svt = nouv;
+        *end = nouv;
+    }
+}
+
 void extractFieldValue(const char *jsonString, const char *fieldName, char *destination)
 {
     char fieldWithQuotes[30];
@@ -41,20 +74,18 @@ void extractFieldValue(const char *jsonString, const char *fieldName, char *dest
     }
 }
 
-// todo creating project functions:end
-
-int main()
+void creation(FILE *fPtr, list *tete)
 {
-    FILE *fPtr;
     list start = NULL, end = NULL;
-    fPtr = fopen("logicalFile.txt", "r");
     char buffer[1000];
     char nom[44], prenom[44], matricule[44], id[44];
+
+    fPtr = fopen("logicalFile.txt", "r");
     for (int i = 0; i < 8; i++)
     {
         fgets(buffer, 1000, fPtr); //! to skip the first 8 lines
     }
-    while (buffer == '{')
+    while (buffer[4] == '{')
     {
         fgets(buffer, 1000, fPtr);
         extractFieldValue(buffer, "nom", nom);
@@ -63,9 +94,97 @@ int main()
         fgets(buffer, 1000, fPtr);
         extractFieldValue(buffer, "matricule", matricule);
         fgets(buffer, 1000, fPtr);
-        extractFieldValue(buffer, "id", id);
+        extractFieldValue(buffer, "ID", id);
+        fgets(buffer, 1000, fPtr);
+        fgets(buffer, 1000, fPtr);
+        append_in_list(&start, &end, nom, prenom, matricule, id);
     }
+    *tete = start;
+}
 
+void insertion(list *start, char nom[30], char prenom[30], char matricule[8], char id[30])
+{
+    int numId = atoi(id);
+    list prd = *start, svt = (*start)->svt;
+    list nouv;
+    if (*start == NULL)
+    {
+        allouer(start);
+        //? set data:start
+        strncat((*start)->nom, nom, sizeof((*start)->nom) - strlen((*start)->nom) - 1);
+        strncat((*start)->prenom, prenom, sizeof((*start)->prenom) - strlen((*start)->prenom) - 1);
+        strncat((*start)->matricule, matricule, sizeof((*start)->matricule) - strlen((*start)->matricule) - 1);
+        strncat((*start)->id, id, sizeof((*start)->id) - strlen((*start)->id) - 1);
 
+        //? set data:end
+        (*start)->svt = NULL;
+    }
+    else if (prd != NULL && svt == NULL)
+    {
+        if (numId < atoi(prd->id))
+        {
+            allouer(&nouv);
+            //? set data:start
+            strncat(nouv->nom, nom, sizeof(nouv->nom) - strlen(nouv->nom) - 1);
+            strncat(nouv->prenom, prenom, sizeof(nouv->prenom) - strlen(nouv->prenom) - 1);
+            strncat(nouv->matricule, matricule, sizeof(nouv->matricule) - strlen(nouv->matricule) - 1);
+            strncat(nouv->id, id, sizeof(nouv->id) - strlen(nouv->id) - 1);
+            //? set data:end
+            nouv->svt = prd;
+            prd->svt = NULL;
+            prd = nouv;
+        }
+        else
+        {
+            allouer(&nouv);
+            //? set data:start
+            strncat(nouv->nom, nom, sizeof(nouv->nom) - strlen(nouv->nom) - 1);
+            strncat(nouv->prenom, prenom, sizeof(nouv->prenom) - strlen(nouv->prenom) - 1);
+            strncat(nouv->matricule, matricule, sizeof(nouv->matricule) - strlen(nouv->matricule) - 1);
+            strncat(nouv->id, id, sizeof(nouv->id) - strlen(nouv->id) - 1);
+            //? set data:end
+            prd->svt = nouv;
+            nouv->svt = NULL;
+        }
+    }
+    else
+    {
+        while (prd != NULL)
+        {
+            if (numId <= atoi(svt->id) && numId > atoi(prd->id))
+            {
+                allouer(&nouv);
+                //? set data:start
+                strncat(nouv->nom, nom, sizeof(nouv->nom) - strlen(nouv->nom) - 1);
+                strncat(nouv->prenom, prenom, sizeof(nouv->prenom) - strlen(nouv->prenom) - 1);
+                strncat(nouv->matricule, matricule, sizeof(nouv->matricule) - strlen(nouv->matricule) - 1);
+                strncat(nouv->id, id, sizeof(nouv->id) - strlen(nouv->id) - 1);
+                //? set data:end
+                nouv->svt=svt;
+                prd->svt=nouv;
+            }
+            prd=prd->svt;
+            svt=svt->svt;
+        }
+    }
+}
+
+void print_list(list start)
+{
+    list current = start;
+    while (current != NULL)
+    {
+        printf("name = %s\nprenom = %s\nmatricule = %s\nid = %s\n", current->nom, current->prenom, current->matricule, current->id);
+        printf("####################################################################\n");
+        current = current->svt;
+    }
+}
+
+int main()
+{
+    FILE *fPtr;
+    list start = NULL, end = NULL;
+    creation(fPtr, &start);
+    print_list(start);
     return 0;
 }
--- test.c
+++ test.c
@@ -1,30 +1,139 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
-void extractFieldValue(char *jsonString, char *fieldName, char *destination) {
-    char *fieldStart = strstr(jsonString, fieldName);
+// Project types
+typedef struct cellule *list;
+typedef struct cellule
+{
+    char nom[30], prenom[30], matricule[8], id[30];
+    list svt;
+} cellule;
 
-    if (fieldStart != NULL) {
-        fieldStart += strlen(fieldName) + 3; // Move to the beginning of the field value
+// Function to allocate memory for a new node
+void allouer(list *nouv)
+{
+    *nouv = (list)malloc(sizeof(cellule));
+    if (*nouv == NULL)
+    {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+}
 
-        char *fieldEnd = strchr(fieldStart, '"'); // Find the end of the field value
+// Function to append a new node to the list
+void append_in_list(list *start, list *end, const char *nom, const char *prenom, const char *matricule, const char *id)
+{
+    list nouv;
+    allouer(&nouv);
 
-        if (fieldEnd != NULL) {
+    // Set data
+    strncat(nouv->nom, nom, sizeof(nouv->nom) - strlen(nouv->nom) - 1);
+    strncat(nouv->prenom, prenom, sizeof(nouv->prenom) - strlen(nouv->prenom) - 1);
+    strncat(nouv->matricule, matricule, sizeof(nouv->matricule) - strlen(nouv->matricule) - 1);
+    strncat(nouv->id, id, sizeof(nouv->id) - strlen(nouv->id) - 1);
+
+    // Set pointers
+    nouv->svt = NULL;
+    if (*start == NULL)
+    {
+        *start = nouv;
+        *end = *start;
+    }
+    else
+    {
+        (*end)->svt = nouv;
+        *end = nouv;
+    }
+}
+
+// Function to extract field value from JSON string
+void extractFieldValue(const char *jsonString, const char *fieldName, char *destination)
+{
+    char fieldWithQuotes[40]; // Increased buffer size
+    snprintf(fieldWithQuotes, sizeof(fieldWithQuotes), "\"%s\"", fieldName);
+
+    char *fieldStart = strcasestr(jsonString, fieldWithQuotes);
+
+    if (fieldStart != NULL)
+    {
+        fieldStart += strlen(fieldWithQuotes) + 3;
+
+        char *fieldEnd = strchr(fieldStart, '"');
+
+        if (fieldEnd != NULL)
+        {
             size_t fieldLength = fieldEnd - fieldStart;
             strncpy(destination, fieldStart, fieldLength);
-            destination[fieldLength] = '\0'; // Null-terminate the field value
+            destination[fieldLength] = '\0';
         }
     }
 }
 
-int main() {
-    char jsonString[] = "{\"name\": \"omar\", \"surname\": \"heriche\", \"value\": \"03\"}";
+int main()
+{
+    // Declaration
+    FILE *fPtr;
+    list start = NULL, end = NULL;
+    fPtr = fopen("logicalFile.txt", "r");
 
-    char nameValue[30]; // Adjust the size as needed
+    if (fPtr == NULL)
+    {
+        perror("Error opening file");
+        return 1;
+    }
 
-    extractFieldValue(jsonString, "\"name\"", nameValue);
+    char buffer[1000];
+    char nom[30], prenom[30], matricule[8], id[30]; // Reduced buffer size
+    // Skip the first 8 lines
+    for (int i = 0; i < 8; i++)
+    {
+        if (fgets(buffer, sizeof(buffer), fPtr) == NULL)
+        {
+            perror("Error reading file");
+            fclose(fPtr);
+            return 1;
+        }
+    }
 
-    printf("Extracted name value: %s\n", nameValue);
+    // Read and append data to the list
+    while (fgets(buffer, sizeof(buffer), fPtr) != NULL && strstr(buffer, "{") != NULL)
+    {
+        fgets(buffer, sizeof(buffer), fPtr);
+        extractFieldValue(buffer, "nom", nom);
+        fgets(buffer, sizeof(buffer), fPtr);
+        extractFieldValue(buffer, "prenom", prenom);
+        fgets(buffer, sizeof(buffer), fPtr);
+        extractFieldValue(buffer, "matricule", matricule);
+        fgets(buffer, sizeof(buffer), fPtr);
+        extractFieldValue(buffer, "ID", id);
+        append_in_list(&start, &end, nom, prenom, matricule, id);
+        fgets(buffer, sizeof(buffer), fPtr); // Skip a line
+        fgets(buffer, sizeof(buffer), fPtr); // Skip the closing bracket
+    }
+
+    // Print the list
+    list current = start;
+    while (current != NULL)
+    {
+        printf("name = %s prenom = %s matricule = %s id = %s\n", current->nom, current->prenom, current->matricule, current->id);
+        current = current->svt;
+    }
+
+    // Cleanup
+    fclose(fPtr);
+
+    // Free allocated memory
+    current = start;
+    while (current != NULL)
+    {
+        list temp = current;
+        current = current->svt;
+        free(temp);
+    }
+
+    printf("\n the end");
 
     return 0;
 }
